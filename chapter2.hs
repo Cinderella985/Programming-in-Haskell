c07c18d5eb5b2e64130483a61a5d3dcfb2a7ebd7
Sherry <cinderella.hyx@gmail.com>
2022-04-17 12:45:51 +0100

    haskell

---
+++ chapter2.hs
@@ -0,0 +1,14 @@
+--2.7.4
+last0 :: [a] -> a
+last0 xs = xs !! (length xs - 1)
+
+last1 :: [a] -> a
+last1 xs = head(reverse xs)
+
+--2.7.5
+init0 :: [a] -> [a]
+init0 xs = take(length xs -1) xs
+
+init1 :: [a] -> [a]
+init1 xs = reverse(tail(reverse xs))
+
